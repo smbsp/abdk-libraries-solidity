1c3116a9e192b92b7190029d3cacf267c22745bd
Stan Marsh <stanmarsh8867@gmail.com>
2022-06-01 15:14:52 +0530

    Sample Tests

--- EchidnaTest.sol
+++ EchidnaTest.sol
@@ -1,52 +1,60 @@
 // SPDX-License-Identifier: BSD-4-Clause
 pragma solidity ^0.8.1;
 
-import "ABDKMath64x64.sol";
+import "./ABDKMath64x64.sol";
 
 contract Test {
-   int128 internal zero = ABDKMath64x64.fromInt(0);
-   int128 internal one = ABDKMath64x64.fromInt(1);
+    int128 internal zero = ABDKMath64x64.fromInt(0);
+    int128 internal one = ABDKMath64x64.fromInt(1);
+    int128 private constant MIN_64x64 = -0x80000000000000000000000000000000;
+    int128 private constant MAX_64x64 = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 
-   event Value(string, int64);
-   function debug(string calldata x, int128 y) public {
-     emit Value(x, ABDKMath64x64.toInt(y));
-   }
- 
-   function add(int128 x, int128 y) public returns (int128) {
-     return ABDKMath64x64.add(x, y);
-   }
+    event Value(string, int64);
 
-   function mul(int128 x, int128 y) public returns (int128) {
-     return ABDKMath64x64.mul(x, y);
-   }
+    // Wrappers
+    function add(int128 x, int128 y) public returns (int128) {
+        return ABDKMath64x64.add(x, y);
+    }
 
-   function div(int128 x, int128 y) public returns (int128) {
-     return ABDKMath64x64.div(x, y);
-   }
+    function sub(int128 x, int128 y) public returns (int128) {
+        return ABDKMath64x64.sub(x, y);
+    }
 
-   function fromInt(int256 x) public returns (int128) {
-     return ABDKMath64x64.fromInt(x);
-   }
+    // Fuzzing - Addition
+    // Adds a number to 0
+    function testAddNumberToZero(int128 x) public {
+        // int128 a = fromInt(x);
+        assert(add(x, 0) == x);
+    }
 
-   function pow(int128 x, uint256 y) public returns (int128) {
-     return ABDKMath64x64.pow(x, y);
-   }
+    // Addition of two numbers must be greater than 0 - wrong assertion
+    function testAddUnsafe(int128 x, int128 y) public {
+        assert(add(x, y) >= x);
+        assert(add(x, y) >= y);
+    }
 
-   function neg(int128 x) public returns (int128) {
-     return ABDKMath64x64.neg(x);
-   }
+    // Addition of two numbers must be bounded
+    function testAddBounds(int128 x, int128 y) public {
+        assert(add(x, y) <= MAX_64x64);
+        assert(add(x, y) >= MIN_64x64);
+    }
 
-   function inv(int128 x) public returns (int128) {
-     return ABDKMath64x64.inv(x);
-   }
+    // Fuzzing - Substraction
+    // Substracts 0 from a number
+    function testSubstractionZero(int128 x) public {
+        // int128 a = fromInt(x);
+        assert(sub(x, 0) == x);
+    }
 
-   function sqrt(int128 x) public returns (int128) {
-     return ABDKMath64x64.sqrt(x);
-   }
+    // Substraction of two numbers must be greater than 0 - wrong assertion
+    function testSubstractionUnsafe(int128 x, int128 y) public {
+        assert(sub(x, y) <= x);
+        assert(sub(x, y) <= y);
+    }
 
-   function testAdd(int128 x, int128 y, int128 z) public {
-     // TODO
-   }
-
-  // TODO: add more functions with assertions
+    // Addition of two numbers must be bounded
+    function testSubstractionBounds(int128 x, int128 y) public {
+        assert(sub(x, y) <= MAX_64x64);
+        assert(sub(x, y) >= MIN_64x64);
+    }
 }
